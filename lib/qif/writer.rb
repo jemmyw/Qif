017932b1cfe63923ba3464f5b9f1211c272b9a50
Jeremy <jemmyw@gmail.com>
2011-03-07 22:38:50 +1300

    Update changelog and writer docs

--- lib/qif/writer.rb
+++ lib/qif/writer.rb
@@ -25,12 +25,28 @@ module Qif
     end
     
     # Create a new Qif::Writer. Expects an IO object or a filepath.
+    # Can optionally take a block which will yield the writer and
+    # automatically call write afterwards.
     #
     # Parameters:
     #
     # * <tt>io</tt> - An IO object or filepath
     # * <tt>type</tt> - Used to write the header, defaults to 'Bank'
     # * <tt>format</tt> - The format of dates in the qif file, defaults to 'dd/mm/yyyy'. Also accepts 'mm/dd/yyyy'
+    #
+    # Usage:
+    #   buffer = StringIO.new
+    #   writer = Qif::Writer.new(buffer)
+    #   writer << Qif::Transaction.new(:date => Time.now, :amount => 10.0, :name => 'Credit')
+    #   writer.write
+    #   puts buffer
+    #
+    # Usage with block:
+    #   buffer = StringIO.new
+    #   Qif::Writer.new(buffer) do |writer|
+    #     writer << Qif::Transaction.new(:date => Time.now, :amount => 10.0, :name => 'Credit')
+    #   end
+    #   puts buffer
     def initialize(io, type = 'Bank', format = 'dd/mm/yyyy')
       @io = io.respond_to?(:write) ? io : File.open(io, 'w')
       @type = type
