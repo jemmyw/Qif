21db3b0c9822c94d06a23cc7f55d76af8161b757
Luiza Pagliari <lpagliar@thoughtworks.com>
2013-07-11 08:54:37 -0300

    Fixing spacing before making changes to date_format

--- lib/qif/date_format.rb
+++ lib/qif/date_format.rb
@@ -3,42 +3,42 @@ require 'time'
 module Qif
   class DateFormat
     attr_reader :format
-    
+
     def initialize(format = 'dd/mm/yyyy')
       @format = format
     end
-    
+
     def parse(date)
       regex = convert_format_to_regex
       order = date_order
-      
+
       if match = regex.match(date)
-				year = match[order.index('y')+1].to_i
-			
-				if year < 100
-					start_year = Time.now.year - 50
-					start_cent = (start_year/100).to_i*100
-					if year > start_year-start_cent
-						year += start_cent
-					else
-						year += ((Time.now.year/100).to_i*100)
-					end
-				end
-        
-				Time.mktime(year, *%w(m d).map{|t| match[order.index(t) + 1].to_i })
+        year = match[order.index('y')+1].to_i
+
+        if year < 100
+          start_year = Time.now.year - 50
+          start_cent = (start_year/100).to_i*100
+          if year > start_year-start_cent
+            year += start_cent
+          else
+            year += ((Time.now.year/100).to_i*100)
+          end
+        end
+
+        Time.mktime(year, *%w(m d).map{|t| match[order.index(t) + 1].to_i })
       end
     end
-    
+
     def format(date)
       date.strftime(convert_format_to_strftime)
     end
-    
+
     private
-    
+
     def date_order
       %w(d m y).sort{|a,b| @format.index(a) <=> @format.index(b) }
     end
-    
+
     def convert_format_to_strftime
       format = @format.dup
       format.gsub!(/d{1,2}/, '%d')
@@ -47,7 +47,7 @@ module Qif
       format.gsub!('yy', '%y')
       format
     end
-    
+
     def convert_format_to_regex
       format = @format.dup
       format.gsub!('dd'   , '([0-3][0-9])')
@@ -56,7 +56,7 @@ module Qif
       format.gsub!('m'    , '([0-1]?[0-9])')
       format.gsub!('yyyy' , '([1-2][0-9]{3})')
       format.gsub!('yy'   , '([0-9]{2})')
-      
+
       /#{format}/mi
     end
   end
